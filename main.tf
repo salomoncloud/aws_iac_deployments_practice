3a527c91cd7cd006c5fba3f5347f87624e72f3fe
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-06-02 12:09:11 -0400

    subnet creation

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "iac_remote_vpc" {
     "Name" = "IaC Production VPC"
   }
 }
+# creating a subnet for my vpc 
+resource "aws_subnet" "iac_web" {
+    vpc_id = aws_vpc.iac_remote_vpc.id
+    cidr_block = "192.168.0.32/27"
+    availability_zone = "us-east-1a"
+    tags = {
+        "Name" = "Iac Web Subnet"
+    }
+}
