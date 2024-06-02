3e7f3ac35d74627a3ecc60d891985b5e1c522c79
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-06-02 12:25:37 -0400

    variables added

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # creating a vpc for my lab
 resource "aws_vpc" "iac_remote_vpc" {
-  cidr_block = "192.168.0.0/24"
+  cidr_block = var.vpc_cidr_block
   tags = {
     "Name" = "IaC Production VPC"
   }
@@ -8,8 +8,8 @@ resource "aws_vpc" "iac_remote_vpc" {
 # creating a subnet for my vpc 
 resource "aws_subnet" "iac_web" {
     vpc_id = aws_vpc.iac_remote_vpc.id
-    cidr_block = "192.168.0.32/27"
-    availability_zone = "us-east-1a"
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.sub_az
     tags = {
         "Name" = "Iac Web Subnet"
     }
