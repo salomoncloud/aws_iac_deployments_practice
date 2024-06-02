1621b801a985c949aa5f654e3c5b0689e39673fa
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-06-02 12:25:25 -0400

    variables

--- variables.tf
+++ variables.tf
@@ -5,3 +5,23 @@ type = string
 variable "aws_secret_access_key" {
 type = string
 }
+
+variable "vpc_cidr_block" {
+    type = string
+    default = "192.168.0.0/24"
+}
+
+variable "subnet_cidr_block" {
+    type = string
+    default = "192.168.0.32/27"
+}
+
+variable "sub_az" {
+    type = string
+    default = "us-east-1a"
+}
+
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+}
