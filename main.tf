bd8a59dadbb45b064f402c08f89c2d2822dd4308
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-06-02 12:51:59 -0400

    rtb and igw added

--- main.tf
+++ main.tf
@@ -14,3 +14,22 @@ resource "aws_subnet" "iac_web" {
         "Name" = "Iac Web Subnet"
     }
 }
+# igw
+resource "aws_internet_gateway" "iac_igw" {
+    vpc_id = aws_vpc.iac_remote_vpc.id 
+    tags = {
+        "Name" = "${var.vpc_name} Internet Gateway"
+    }
+}
+# Route Table
+resource "aws_default_route_table" "iac_route_table" {
+    aws_default_route_table_id = aws_vpc.iac_remote_vpc.aws_default_route_table_id
+    # default route that will handle all traffic not explicitly known by the route table, in other words for the igw to the internet not local
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.iac_igw.id
+    }
+    tags = {
+        "Name" = "IaC Route Table"
+    }
+}
