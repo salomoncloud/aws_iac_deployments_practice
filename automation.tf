d6a1ff5663ff19fdef61f24a64ade1f6242b5825
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-06-07 19:19:37 -0400

    automation for the ssh key

--- automation.tf
+++ automation.tf
@@ -1,3 +1,4 @@
+/*
 ## File Provisioner: Copies the terraform-key.pem file to /tmp/terraform-key.pem
   provisioner "file" {
     source      = "salomoncloud/aws_iac_deployments_practice/iac_key_pair.pem"
